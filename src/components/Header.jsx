0337b18b0e787b370fd0c4b8ca05f6dd74f5e73f
AdRiAnPg7 <adriancracker@gmail.com>
2021-07-22 20:31:29 -0400

    Particles Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,32 @@ import React from 'react';
 import Typed from 'react-typed';
 // Styles
 import '../styles/Header.css';
+
+import ParticlesDesign from './Particles';
+
 export default function Header() {
     return (
-        <div className="header-wraper">
-            <div className="main-info">
-                <h1>Web development and websites promotions</h1>
-                <Typed 
-                    className="typed-text"
-                    strings={['Web Design', 'Web Development', 'Automation', 'UI/UX']}
-                    typeSpeed={40}
-                    backSpeed={60}
-                    loop
-                />
-                <a href="#" className="btn-main">Contact Me</a>
+        <>
+            <ParticlesDesign/>
+            <div className="header-wraper">
+                <div className="main-info">
+                    <h1>Web development and websites promotions</h1>
+                    <Typed
+                        className="typed-text"
+                        strings={[
+                            'Web Design',
+                            'Web Development',
+                            'Mobile Design',
+                            'Automation',
+                            'Testing'
+                        ]}
+                        typeSpeed={40}
+                        backSpeed={60}
+                        loop
+                    />
+                    <a href="#" className="btn-main">Contact Me</a>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
